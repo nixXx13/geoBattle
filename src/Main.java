9435ee164f978473c26197443cdb42b0c36be80f
Nir <nirose@gmail.com>
2018-12-23 18:52:48 +0200

    23.12

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -8,22 +9,6 @@ public class Main {
         MainManager mainManager = new MainManager();
         mainManager.start();
 
-    }
-
-    /*
-    TODO - Fetch data from DB
-     */
-    static void getData(List<GameData> data, List<String> answers, int numPlayers) {
-        for(int i = 0 ; i<4 ; i++) {
-            GameData questionGameData = new GameData(GameData.DataType.QUESTION);
-            questionGameData.setContent("question", "1+"+i);
-            questionGameData.setContent("pAnswer0", ""+i);
-            questionGameData.setContent("pAnswer1", ""+(i+1));
-            questionGameData.setContent("pAnswer2", "" +(i+2));
-            questionGameData.setContent("pAnswer3", "" +(i+3));
-            data.add(questionGameData);
-            answers.add(String.valueOf(i+1));
-        }
     }
 
 }
